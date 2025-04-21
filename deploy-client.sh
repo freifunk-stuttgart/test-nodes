2f4a7fe2642ddd2a48ff5db84700a794c97e4bd8
root <root@ffs10.freifunk-stuttgart.de>
2025-04-21 09:27:07 +0200

    add help to deploy-client.sh

--- deploy-client.sh
+++ deploy-client.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-NAME=$1
-ID=$2
+set -eu
+
+NAME="${1:-}"
+ID="${2:-}"
+
+if [ -z "$NAME" ] || [ -z "$ID" ]; then
+	echo usage: $0 NAME ID
+	echo Tip: run mkclientconfig.py to generate a commandline for this script
+	exit 1
+fi
+
 VMID=9$ID
 source `hostname`.conf
-pct stop $VMID
-pct destroy $VMID
+pct stop $VMID || true
+pct destroy $VMID || true
 pct create $VMID $CT_TEMPLATE --features nesting=1 --storage ${ZP} \
 	--net0 name=eth0,bridge=${BR_CLIENT},ip=dhcp,ip6=auto,tag=$ID,type=veth \
 	--memory 512 --hostname $NAME
